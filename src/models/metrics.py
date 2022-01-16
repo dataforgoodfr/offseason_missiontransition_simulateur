759427cf8cae70e34fe9877daa5807cf604df974
cgoudet <goudetchristophe@gmail.com>
2022-01-16 21:30:16 +0100

    add test_relative_attribution_curve

--- src/models/metrics.py
+++ src/models/metrics.py
@@ -1,20 +1,21 @@
 import numpy as np
+from sklearn.utils.validation import check_array
 
 
-def dfill(a):
+def dfill(a: np.ndarray) -> np.ndarray:
     n = a.size
     b = np.concatenate([[0], np.where(a[:-1] != a[1:])[0] + 1, [n]])
     return np.arange(n)[b[:-1]].repeat(np.diff(b))
 
 
-def argunsort(s):
+def argunsort(s: np.ndarray) -> np.ndarray:
     n = s.size
     u = np.empty(n, dtype=np.int64)
     u[s] = np.arange(n)
     return u
 
 
-def cumcount(a):
+def cumcount(a: np.ndarray) -> np.ndarray:
     """
     Count the cumulative number of occurence of element at each position
     """
@@ -25,8 +26,27 @@ def cumcount(a):
     return (np.arange(n) - dfill(b))[i]
 
 
-def uniquecount(a):
+def uniquecount(a: np.ndarray) -> np.ndarray:
     """
     Count the cumulative number of unique values of the array
     """
     return (cumcount(a) == 0).cumsum()
+
+
+def relative_attribution_curve(
+    first: np.ndarray, second: np.ndarray, score: np.ndarray
+) -> (np.ndarray, np.ndarray, np.ndarray):
+    """
+    Return the relative proportion of unique elements matched per dataset,
+    as a funciton of the score.
+    """
+    first = check_array(first, dtype=None, ensure_2d=False)
+    second = check_array(second, dtype=None, ensure_2d=False)
+    score = check_array(score, ensure_2d=False)
+
+    sorted_idx = np.argsort(score, kind="mergesort")[::-1]
+
+    th = score[sorted_idx]
+    first = uniquecount(first[sorted_idx]) / len(np.unique(first))
+    second = uniquecount(second[sorted_idx]) / len(np.unique(second))
+    return first, second, th
--- tests/data_fetching/models/test_metrics.py
+++ tests/data_fetching/models/test_metrics.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from src.models.metrics import cumcount, uniquecount
+from src.models.metrics import cumcount, relative_attribution_curve, uniquecount
 
 
 def test_cumcount():
@@ -15,3 +15,14 @@ def test_uniquecount():
     out = uniquecount(inp)
     exp = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4]
     np.testing.assert_array_equal(out, exp)
+
+
+def test_relative_attribution_curve():
+    score = np.array([0, 2, 1, -1])
+    first = np.array(list("abab"))
+    second = np.array(list("bdeb"))
+    rate_first, rate_second, th = relative_attribution_curve(first, second, score)
+
+    np.testing.assert_array_equal(th, [2, 1, 0, -1])
+    np.testing.assert_array_equal(rate_first, [0.5, 1, 1, 1])
+    np.testing.assert_array_equal(rate_second, [1 / 3, 2 / 3, 1, 1])
