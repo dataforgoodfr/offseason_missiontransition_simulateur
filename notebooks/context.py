5f380e3e3aa4b776dffce8a4122b67ce36f7d0b6
cgoudet <goudetchristophe@gmail.com>
2022-01-16 22:19:23 +0100

    Add FuzzyMatching

---
+++ notebooks/context.py
@@ -0,0 +1,10 @@
+import sys
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+sys.path.insert(0, str(Path(__file__).parents[1]))
+
+from src.config import Config
