b904cea6a67531e420043361fd6a8d7fe37bc44b
cgoudet <goudetchristophe@gmail.com>
2022-01-19 23:23:14 +0100

    update tests matching

--- src/data_processing/matching_ademe_mt.py
+++ src/data_processing/matching_ademe_mt.py
@@ -1,14 +1,20 @@
+import logging
+
+import numpy as np
 import pandas as pd
 from thefuzz import fuzz
-from tqdm import tqdm
 
 from src.config import Config
+
+logger = logging.getLogger(__name__)
 
 
 def load_and_process_ademe():
     if not (Config.INTDIR / "ademe.parquet").exists():
         raise RuntimeError("Missing ADEME data")
-    df_ademe = pd.read_parquet(Config.INTDIR / "ademe.parquet")
+    df_ademe = pd.read_parquet(
+        Config.INTDIR / "ademe.parquet", columns=["projet", "projet_md5"]
+    )
     return df_ademe
 
 
@@ -16,64 +22,47 @@ def load_and_process_mission_transition():
     if not (Config.INTDIR / "mission_transition.parquet").exists():
         raise RuntimeError("Missing Mission Transition data")
     df_mission_transition = pd.read_parquet(
-        Config.INTDIR / "mission_transition.parquet"
+        Config.INTDIR / "mission_transition.parquet",
+        columns=["source", "name", "funder_name"],
     ).pipe(lambda df: df[df["funder_name"] == "ADEME"])
     return df_mission_transition
 
 
-def compute_score(df_product):
+def fuzzy_score(first: str, second: str) -> float:
     return max(
         [
-            fuzz.partial_ratio(df_product["name"], df_product["project"]),
-            fuzz.token_set_ratio(df_product["name"], df_product["project"]),
-            fuzz.token_sort_ratio(df_product["name"], df_product["project"]),
+            fuzz.partial_ratio(first, second),
+            fuzz.token_set_ratio(first, second),
+            fuzz.token_sort_ratio(first, second),
         ]
     )
 
 
+v_fuzzy_score = np.vectorize(fuzzy_score)
+
+
 def make_matching(df_product, threshold):
-    tqdm.pandas()
-    df_product["score"] = df_product.progress_apply(compute_score, axis=1)
-    df_selected = df_product.loc[
-        df_product["score"] > threshold, ["index_ademe", "index_mt"]
-    ].reset_index(drop=True)
-    return df_selected
-
-
-def make_final_dataframe(df_selected, df_ademe, df_mission_transition):
-    df_selected_with_ademe = pd.merge(
-        df_selected, df_ademe, left_on="index_ademe", right_index=True
-    )
-
-    final_df = pd.merge(
-        df_selected_with_ademe,
-        df_mission_transition,
-        left_on="index_mt",
-        right_index=True,
-    ).reset_index(drop=True)
-    return final_df
+    return df_product.assign(
+        score=lambda df: v_fuzzy_score(df["name"], df["projet"])
+    ).pipe(lambda df: df[df["score"] > threshold])
 
 
 def run_matching():
-    # Loading data
     df_ademe = load_and_process_ademe()
     df_mission_transition = load_and_process_mission_transition()
 
-    df_product = (
-        df_ademe[["project"]]
-        .reset_index()
-        .merge(
-            df_mission_transition[["name"]].reset_index(),
-            how="cross",
-            suffixes=["_ademe", "_mt"],
-        )
+    df_product = df_ademe.merge(df_mission_transition, how="cross").pipe(
+        make_matching, Config.MATCHING_THRESHOLD
+    )[["source", "projet_md5"]]
+    logger.info(
+        "matching_ademe_mt",
+        extra=dict(
+            size=len(df_product),
+            ademe_unique=df_product["projet_md5"].nunique(),
+            mt_unique=df_product["source"].nunique(),
+        ),
     )
-
-    df_selected = make_matching(df_product, Config.MATCHING_THRESHOLD)
-
-    final_df = make_final_dataframe(df_selected, df_ademe, df_mission_transition)
-
-    final_df.to_parquet(Config.INTDIR / "ademe_mt.parquet")
+    df_product.to_parquet(Config.INTDIR / "ademe_mt.parquet")
 
 
 if __name__ == "__main__":
--- tests/data_processing/test_matching_ademe_mt.py
+++ tests/data_processing/test_matching_ademe_mt.py
@@ -1,23 +1,8 @@
 from pathlib import Path
 
 import pandas as pd
-import pytest
 
-from src.data_processing.matching_ademe_mt import make_final_dataframe, make_matching
-
-
-@pytest.fixture()
-def dataframes():
-    df_ademe = pd.read_csv(
-        Path(__file__).parent / "fixtures" / "matching_ademe_mt" / "df_ademe.csv",
-    )
-    df_mission_transition = pd.read_csv(
-        Path(__file__).parent
-        / "fixtures"
-        / "matching_ademe_mt"
-        / "df_mission_transition.csv",
-    )
-    yield df_ademe, df_mission_transition
+from src.data_processing.matching_ademe_mt import make_matching
 
 
 class TestMatchingAdemeMissionTransition:
@@ -25,7 +10,9 @@ class TestMatchingAdemeMissionTransition:
         df_product = pd.read_csv(
             Path(__file__).parent / "fixtures" / "matching_ademe_mt" / "df_product.csv",
         )
-        df_selected = make_matching(df_product, 75)
+        df_selected = make_matching(df_product, 75)[
+            ["projet_md5", "source"]
+        ].reset_index(drop=True)
 
         true_df_selected = pd.read_csv(
             Path(__file__).parent
@@ -35,17 +22,3 @@ class TestMatchingAdemeMissionTransition:
         )
 
         pd.testing.assert_frame_equal(df_selected, true_df_selected)
-
-    def test_make_final(self, dataframes):
-        df_selected = pd.read_csv(
-            Path(__file__).parent / "fixtures" / "matching_ademe_mt" / "df_selected.csv"
-        )
-        df_ademe, df_mission_transition = dataframes
-        final_df = make_final_dataframe(df_selected, df_ademe, df_mission_transition)
-
-        true_final_df = pd.read_csv(
-            Path(__file__).parent / "fixtures" / "matching_ademe_mt" / "final_df.csv",
-        )
-
-        pd.testing.assert_series_equal(final_df["project"], true_final_df["project"])
-        pd.testing.assert_series_equal(final_df["name"], true_final_df["name"])
