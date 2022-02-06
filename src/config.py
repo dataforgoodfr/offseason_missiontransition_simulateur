ec09f1eee5ccd53019bda64747a359476365229a
cgoudet <goudetchristophe@gmail.com>
2022-02-06 11:25:22 +0100

    first baseline

--- src/config.py
+++ src/config.py
@@ -19,6 +19,12 @@ class Config:
     RAWDIR = Path(os.environ.get("RAWDIR", ROOTDIR / "data" / "raw"))
     INTDIR = Path(os.environ.get("INTDIR", ROOTDIR / "data" / "interim"))
     PROCDIR = Path(os.environ.get("INTDIR", ROOTDIR / "data" / "interim"))
+    MODELCONFDIR = Path(
+        os.environ.get("MODELCONFDIR", ROOTDIR / "references" / "models")
+    )
     MATCHING_THRESHOLD = 75
     DB_URI = os.environ.get("DB_URI", str(INTDIR / "mission-transition.sqlite"))
     SIRENE_FEATURES = ["siret", "naf_etab_code"]
+
+    # Features to use as categories in lightgbm and catboost
+    CATEGORIES = ["naf8", "naf8_etab"]
